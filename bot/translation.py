e6e8afeb05f8a47c45fbd23b62124bf9fc3b96a6
msraj1584 <62322316+msraj1584@users.noreply.github.com>
2021-06-19 12:53:20 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+This Bot Belongs to @msraj_movies for searching movies 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@BabyOptimusPrime
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@BabyOptimusPrime
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/BabyOptimusPrime">Baby Optimus Prime</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/msraj1584/Adv-Filter-Bot">Click Me</a></i>
 """
